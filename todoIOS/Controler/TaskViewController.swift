a8e6d2842b45a6718e921ce67103d3dda9330360
driuha99 <lucianrotari@icloud.com>
2018-12-17 23:53:14 +0000

    TableView Delegate Methods modified - added deselectRow on tap

--- todoIOS/Controler/TaskViewController.swift
+++ todoIOS/Controler/TaskViewController.swift
@@ -14,15 +14,15 @@ class TaskViewController:  UIViewController, UITableViewDelegate, UITableViewDat
     var todoTasks: [String] = [String]()
     
     
-    // Mark: Cell Identifier constant
+    // Mark: - Cell Identifier constant
     let cellID = "cellID"
     
-    // Mark: IBOutlets
+    // Mark: - IBOutlets
     @IBOutlet weak var actionTextField: UITextField!
     @IBOutlet weak var tasksTableViews: UITableView!
     
     
-    // Mark: Collection of the tasks
+    // Mark: - Collection of the tasks
    
 
     override func viewDidLoad() {
@@ -31,18 +31,18 @@ class TaskViewController:  UIViewController, UITableViewDelegate, UITableViewDat
         customizeUITableView()
         
         
-        //TODO: Set yourself as the delegate and datasource here:
+        //TODO: - Set yourself as the delegate and datasource here:
         tasksTableViews.delegate = self
         tasksTableViews.dataSource = self 
 
-        //MARK:  Registers a class for use in creating new table cells.
+        //MARK: -  Registers a class for use in creating new table cells.
         tasksTableViews.register(UITableViewCell.self, forCellReuseIdentifier: cellID)
 
         
     }
 
     
-    // TODO: Use this method or delete it when you will create a text field
+    // MARK: - Customize TextField
     func actionTextFieldCustomizeUI() {
         
         // Create padding for the Text Field placeholder
@@ -52,7 +52,8 @@ class TaskViewController:  UIViewController, UITableViewDelegate, UITableViewDat
         actionTextField.leftViewMode = UITextField.ViewMode.always
     }
     
-    // MARK: Customize UITableView
+    
+    // MARK: - Customize UITableView
     func customizeUITableView() {
         // Clear the separator lines betwen cells
         tasksTableViews.separatorStyle = .none
@@ -71,7 +72,8 @@ class TaskViewController:  UIViewController, UITableViewDelegate, UITableViewDat
     }
     
    
-    // MARK: Configuring the Table View
+    // MARK: Table View Data Source Methods
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return todoTasks.count
     }
@@ -83,15 +85,21 @@ class TaskViewController:  UIViewController, UITableViewDelegate, UITableViewDat
              cell.backgroundColor = UIColor(red:0.93, green:0.94, blue:0.95, alpha:1.0)
         }
         
-        
-       
         cell.textLabel?.text = todoTasks[indexPath.row]
         
         return cell
     }
     
     
-    // MARK: Action, add tasks into the table
+    //MARK: - TableView  Delegate Methods
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+    
+    
+   // MARK: - Add New Tasks
     
     @IBAction func addTask(_ sender: UIButton) {
         actionTextField.endEditing(true)
